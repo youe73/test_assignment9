20b1be8525bee86df5da11bf24eb648206480856
youe73 <40825848+youe73@users.noreply.github.com>
2019-04-07 12:03:08 +0200

    Add files via upload

---
+++ main/java/dropletdotnet.java
@@ -0,0 +1,17 @@
+public class dropletdotnet {
+
+    String name;
+    int id;
+    int age;
+    int [] courses;
+
+    public dropletdotnet(int id,  String name, int age, int []courses)
+    {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        this.courses = courses;
+    }
+
+
+}
---
+++ main/java/selenium.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static java.lang.System.out;
+import static java.lang.System.setProperty;
+
+public class selenium {
+    public static void main(String[] args) {
+
+    /*    setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://104.248.249.47/Student/Get");
+
+
+        out.println("Page title is: " + driver.getTitle());
+
+        driver.quit();
+        */
+    }
+}
+
